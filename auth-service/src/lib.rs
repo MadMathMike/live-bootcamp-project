90b2e797e0b189060ac0403472e0e6b7e89226ee
Michael Peterson <MadMathMike@gmail.com>
2025-07-19 13:57:07 -0600

    [Sprint 1][Task 05] feat: Create skeleton requests for remaining auth calls

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 
-use axum::{serve::Serve, Router};
+use axum::{response::IntoResponse, serve::Serve, Router};
+use reqwest::StatusCode;
 use tower_http::services::ServeDir;
 
 // This struct encapsulates our application-related logic.
@@ -15,8 +16,11 @@ impl Application {
     pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
         let router = Router::new()
             .nest_service("/", ServeDir::new("assets"))
-            // TODO: Remove "hello world" handler once a meaningful path has been added
-            .route("/hello", axum::routing::get(hello_handler)); 
+            .route("/signup", axum::routing::post(signup_handler))
+            .route("/login", axum::routing::post(login_handler))
+            .route("/verify-2fa", axum::routing::post(verify_2fa_handler))
+            .route("/logout", axum::routing::post(logout_handler))
+            .route("/verify-token", axum::routing::post(verify_token_handler)); 
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         println!("listening on {}", listener.local_addr().unwrap());
@@ -36,6 +40,22 @@ impl Application {
     }
 }
 
-async fn hello_handler() -> axum::response::Html<&'static str> {
-    axum::response::Html("<h1>Hello, Universe!</h1>")
+async fn signup_handler() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn login_handler() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_2fa_handler() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn logout_handler() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_token_handler() -> impl IntoResponse {
+    StatusCode::OK.into_response()
 }
