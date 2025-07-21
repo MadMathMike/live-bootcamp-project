e58270dc210710baa62e625a6afb6d5326abb7a8
Michael Peterson <MadMathMike@gmail.com>
2025-07-21 17:42:27 -0600

    Add basic JSON validation to POST /signup endpoint

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,10 +1,32 @@
-use crate::helpers::TestApp;
+use crate::helpers::{TestApp};
 
 #[tokio::test]
-async fn signup_returns_ok() {
+async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
 
-    let response = app.post_signup().await;
+    //let random_email = get_random_email(); // Call helper method to generate email
 
-    assert_eq!(response.status().as_u16(), 200);
+    let test_cases = [
+        serde_json::json!("garbage"), // String literals are not valid object literal
+        serde_json::json!([]), // Array literals are not valie object literals
+        serde_json::json!({}), // Empty object
+        serde_json::json!({ // Missing email
+            "password": "password123",
+            "requires2FA": true
+        }),
+        serde_json::json!({ // Missing requires2FA
+            "email": "garbage",
+            "password": "password123"
+        }),
+        serde_json::json!({ // Missing password
+            "email": "garbage",
+            "requires2FA": true
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await;
+        let status_code =  response.status().as_u16();
+        assert_eq!(status_code, 422, "Failed for input: {:?}", test_case);
+    }
 }
