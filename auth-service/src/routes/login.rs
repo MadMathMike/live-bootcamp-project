4d3d0fbfa061f49b58c1f1f8d392bfe8075078ac
Michael Peterson <MadMathMike@gmail.com>
2025-08-02 11:51:58 -0600

    [Sprint 3][Task 01] feat: Implement login route error cases

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,5 +1,30 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use serde::Deserialize;
 
-pub async fn login() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+use crate::{app_state::AppState, domain::{AuthAPIError, Email, Password}};
+
+pub async fn login(
+    State(state): State<AppState>,
+    Json(request): Json<LoginRequest>,
+) -> Result<impl IntoResponse, AuthAPIError> {
+    let email =
+        Email::parse(request.email.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
+    let password =
+        Password::parse(request.password.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
+    
+    if let Ok(user) = state.user_store.read().await.get_user(&email).await {
+        if user.password == password {
+            Ok(StatusCode::OK)
+        } else {
+            Err(AuthAPIError::IncorrectCredentials)    
+        }
+    } else {
+        Err(AuthAPIError::IncorrectCredentials)
+    }
+}
+
+#[derive(Deserialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
 }
--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -1,10 +1,97 @@
-use crate::helpers::TestApp;
+use crate::helpers::{get_random_email, TestApp};
+use auth_service::ErrorResponse;
 
 #[tokio::test]
-async fn login_should_return_200() {
+async fn should_return_422_if_malformed_credentials() {
     let app = TestApp::new().await;
 
-    let response = app.post_login().await;
+    // TODO: add a valid user to the test app first?
 
-    assert_eq!(response.status().as_u16(), 200);
+    let test_cases = [
+        // empty JSON
+        serde_json::json!({}),
+        // missing password
+        serde_json::json!({
+            "email": "test@example.com"
+        }),
+        // missing email
+        serde_json::json!({
+            "password": "password123"
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_login(test_case).await;
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
+}
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+
+    let input = [
+        serde_json::json!({
+            "email": "",
+            "password": "password123"
+        }),
+        serde_json::json!({
+            "email": "invalid_email",
+            "password": "password123"
+        }),
+        serde_json::json!({
+            "email": random_email,
+            "password": ""
+        }),
+        serde_json::json!({
+            "email": "",
+            "password": ""
+        }),
+        serde_json::json!({
+            "email": random_email,
+            "password": "invalid"
+        }),
+    ];
+
+    for i in input.iter() {
+        let response = app.post_login(i).await;
+        assert_eq!(response.status().as_u16(), 400, "Failed for input: {:?}", i);
+
+        assert_eq!(
+            response
+                .json::<ErrorResponse>()
+                .await
+                .expect("Could not deserialize response body to ErrorResponse")
+                .error,
+            "Invalid credentials".to_owned()
+        );
+    }
+}
+
+#[tokio::test]
+async fn should_return_401_if_incorrect_credentials() {
+    let app = TestApp::new().await;
+    let login_body = serde_json::json!({
+        "email": "test@example.com",
+        "password": "password123"
+    });
+
+    let response = app.post_login(&login_body).await;
+    assert_eq!(response.status().as_u16(), 401);
+
+    assert_eq!(
+        response
+            .json::<ErrorResponse>()
+            .await
+            .expect("Could not deserialize response body to ErrorResponse")
+            .error,
+        "Unauthorized".to_owned()
+    );
 }
