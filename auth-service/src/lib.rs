588d762080306003464070595ef66548b35429bc
Michael Peterson <MadMathMike@gmail.com>
2025-08-23 14:35:55 -0600

    [Sprint 6][Task 02] feat: Idiomatic errors - part 1

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -10,7 +10,8 @@ use axum::{
 };
 use domain::AuthAPIError;
 use redis::{Client, RedisResult};
-use routes::{login, logout, signup, verify_2fa, verify_token};
+// use routes::{login, logout, signup, verify_2fa, verify_token};
+use routes::{signup, verify_token};
 use serde::{Deserialize, Serialize};
 use sqlx::{postgres::PgPoolOptions, PgPool};
 use tower_http::{cors::CorsLayer, services::ServeDir, trace::TraceLayer};
@@ -44,9 +45,9 @@ impl Application {
         let router = Router::new()
             .nest_service("/", ServeDir::new("assets"))
             .route("/signup", post(signup))
-            .route("/login", post(login))
-            .route("/verify-2fa", post(verify_2fa))
-            .route("/logout", post(logout))
+            // .route("/login", post(login))
+            // .route("/verify-2fa", post(verify_2fa))
+            // .route("/logout", post(logout))
             .route("/verify-token", post(verify_token))
             .with_state(app_state)
             .layer(cors)
@@ -77,6 +78,8 @@ pub struct ErrorResponse {
 
 impl IntoResponse for AuthAPIError {
     fn into_response(self) -> Response {
+        log_error_chain(&self);
+
         let (status, error_message) = match self {
             AuthAPIError::UserAlreadyExists => (StatusCode::CONFLICT, "User already exists"),
             AuthAPIError::InvalidCredentials => (StatusCode::BAD_REQUEST, "Invalid credentials"),
@@ -85,7 +88,7 @@ impl IntoResponse for AuthAPIError {
             }
             AuthAPIError::MissingToken => (StatusCode::BAD_REQUEST, "Missing auth token"),
             AuthAPIError::InvalidToken => (StatusCode::UNAUTHORIZED, "Invalid auth token"),
-            AuthAPIError::UnexpectedError => {
+            AuthAPIError::UnexpectedError(_) => {
                 (StatusCode::INTERNAL_SERVER_ERROR, "Unexpected error")
             }
         };
@@ -103,4 +106,18 @@ pub async fn get_postgres_pool(url: &str) -> Result<PgPool, sqlx::Error> {
 pub fn get_redis_client(redis_hostname: String) -> RedisResult<Client> {
     let redis_url = format!("redis://{}/", redis_hostname);
     redis::Client::open(redis_url)
+}
+
+fn log_error_chain(e: &(dyn Error + 'static)) {
+    let separator =
+        "\n-----------------------------------------------------------------------------------\n";
+    let mut report = format!("{}{:?}\n", separator, e);
+    let mut current = e.source();
+    while let Some(cause) = current {
+        let str = format!("Caused by:\n\n{:?}", cause);
+        report = format!("{}\n{}", report, str);
+        current = cause.source();
+    }
+    report = format!("{}\n{}", report, separator);
+    tracing::error!("{}", report);
 }
--- auth-service/src/utils/tracing.rs
+++ auth-service/src/utils/tracing.rs
@@ -1,15 +1,26 @@
+use color_eyre::eyre::Result;
+use tracing_error::ErrorLayer;
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::{fmt, EnvFilter};
+
 use std::time::Duration;
 
 use axum::{body::Body, extract::Request, response::Response};
 use tracing::{Level, Span};
 
-pub fn init_tracing() {
-    tracing_subscriber::fmt()
-        .compact()
-        .with_max_level(tracing::Level::DEBUG)
-        .init();
-}
+pub fn init_tracing() -> Result<()> {
+    let fmt_layer = fmt::layer().compact();
 
+    let filter_layer = EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info"))?;
+
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer)
+        .with(ErrorLayer::default()) 
+        .init();
+
+    Ok(())
+}
 // Creates a new tracing span with a unique request ID for each incoming request.
 // This helps in tracking and correlating logs for individual requests.
 pub fn make_span_with_request_id(request: &Request<Body>) -> Span {
