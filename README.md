4ea0896d4dcad39b2ea75c5c94173b21726bb44a
Michael Peterson <MadMathMike@gmail.com>
2025-08-23 10:54:16 -0600

    [Sprint 6][Task 01] feat: Set up tracing

--- README.md
+++ README.md
@@ -32,3 +32,9 @@ visit http://localhost:3000
 ```
 
 visit http://localhost:8000 and http://localhost:3000
+
+## Dependencies
+```bash
+docker run --name ps-db -e POSTGRES_PASSWORD=[YOUR_POSTGRES_PASSWORD] -p 5432:5432 -d postgres:15.2-alpine
+docker run --name redis-db -p "6379:6379" -d redis:7.0-alpine
+```
--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -13,7 +13,10 @@ use redis::{Client, RedisResult};
 use routes::{login, logout, signup, verify_2fa, verify_token};
 use serde::{Deserialize, Serialize};
 use sqlx::{postgres::PgPoolOptions, PgPool};
-use tower_http::{cors::CorsLayer, services::ServeDir};
+use tower_http::{cors::CorsLayer, services::ServeDir, trace::TraceLayer};
+use tracing::info;
+
+use crate::utils::tracing::{make_span_with_request_id, on_request, on_response};
 
 pub mod app_state;
 pub mod domain;
@@ -46,7 +49,13 @@ impl Application {
             .route("/logout", post(logout))
             .route("/verify-token", post(verify_token))
             .with_state(app_state)
-            .layer(cors);
+            .layer(cors)
+            .layer(
+                TraceLayer::new_for_http()
+                    .make_span_with(make_span_with_request_id)
+                    .on_request(on_request)
+                    .on_response(on_response),
+            );
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
@@ -56,7 +65,7 @@ impl Application {
     }
 
     pub async fn run(self) -> Result<(), std::io::Error> {
-        println!("listening on {}", &self.address);
+        info!("listening on {}", &self.address);
         self.server.await
     }
 }
